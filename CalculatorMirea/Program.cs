6424be2e45cd0b3bf3ec53343cd0560c9b4ad6f3
Maksim <maksmor85@gmail.com>
2025-09-20 17:25:12 +0300

    Разрешение конфликтов слияния

--- CalculatorMirea/Program.cs
+++ CalculatorMirea/Program.cs
@@ -6,6 +6,26 @@ class Calculator
 {
     static double memory = 0;
 
+    // Функция для проверки продолжения работы
+    static bool AskToContinue()
+    {
+        Write("Хотите продолжить? (да/нет): ");
+        string answer = ReadLine().ToLower();
+        return (answer == "да" || answer == "д" || answer == "yes" || answer == "y");
+    }
+
+    // Функция для безопасного ввода числа
+    static bool TryReadNumber(out double result)
+    {
+        string input = ReadLine();
+        if (!double.TryParse(input, out result))
+        {
+            WriteLine("Ошибка: введите корректное число");
+            return false;
+        }
+        return true;
+    }
+
     // Функция для бинарных операций
     static double BinaryOperation(double firstNum, char mathOperation, double secondNum)
     {
@@ -46,10 +66,8 @@ class Calculator
         if (!error)
         {
             result = Round(result, 3);
-            return result;
-            WriteLine(result);
         }
-        return double.NaN;
+        return error ? double.NaN : result;
     }
 
     // Функция для унарных операций
@@ -113,10 +131,8 @@ class Calculator
         if (!error)
         {
             result = Round(result, 3);
-            return result;
-            WriteLine(result);
         }
-        return double.NaN;
+        return error ? double.NaN : result;
     }
 
     static void Main()
@@ -130,16 +146,39 @@ class Calculator
             Write("Выберите вид операции бинарная/унарная: ");
             string typeOfOperation = ReadLine().ToLower();
 
+            if (typeOfOperation != "унарная" && typeOfOperation != "бинарная")
+            {
+                WriteLine("Неверный тип операции. Пожалуйста, введите 'унарная' или 'бинарная'.");
+                isContinue = AskToContinue();
+                Clear();
+                continue;
+            }
+
             if (typeOfOperation == "унарная")
             {
                 Write("Введите операцию (1/x, x^2, sqrt(x), M+, M-, MR, MC): ");
                 string mathOption1 = ReadLine().ToLower();
 
+                // Проверка корректности операции
+                string[] validUnaryOperations = { "1/x", "x^2", "sqrt(x)", "m+", "m-", "mr", "mc" };
+                if (Array.IndexOf(validUnaryOperations, mathOption1) == -1)
+                {
+                    WriteLine("Неизвестная операция");
+                    isContinue = AskToContinue();
+                    Clear();
+                    continue;
+                }
+
                 double num = 0;
                 if (mathOption1 != "mr" && mathOption1 != "mc")
                 {
                     Write("Введите число: ");
-                    num = Convert.ToDouble(ReadLine());
+                    if (!TryReadNumber(out num))
+                    {
+                        isContinue = AskToContinue();
+                        Clear();
+                        continue;
+                    }
                 }
 
                 result = UnaryOperation(num, mathOption1);
@@ -153,14 +192,40 @@ class Calculator
             else
             {
                 Write("Введите первое число: ");
-                double firstNum = Convert.ToDouble(ReadLine());
+                if (!TryReadNumber(out double firstNum))
+                {
+                    isContinue = AskToContinue();
+                    Clear();
+                    continue;
+                }
 
                 Write("Введите операцию (+, -, /, *, %): ");
                 string input = ReadLine();
-                char mathOption2 = input.Length > 0 ? input[0] : '\0';
+                if (input.Length != 1)
+                {
+                    WriteLine("Ошибка: введите один символ операции");
+                    isContinue = AskToContinue();
+                    Clear();
+                    continue;
+                }
+
+                char mathOption2 = input[0];
+                char[] validBinaryOperations = { '+', '-', '*', '/', '%' };
+                if (Array.IndexOf(validBinaryOperations, mathOption2) == -1)
+                {
+                    WriteLine("Неизвестная операция");
+                    isContinue = AskToContinue();
+                    Clear();
+                    continue;
+                }
 
                 Write("Введите второе число: ");
-                double secondNum = Convert.ToDouble(ReadLine());
+                if (!TryReadNumber(out double secondNum))
+                {
+                    isContinue = AskToContinue();
+                    Clear();
+                    continue;
+                }
 
                 result = BinaryOperation(firstNum, mathOption2, secondNum);
 
@@ -179,14 +244,7 @@ class Calculator
             }
 
             // Спрашиваем у пользователя хочет ли он продолжить
-            Write("Хотите продолжить? (да/нет): ");
-            string answer = ReadLine().ToLower();
-
-            if (answer != "да" && answer != "д" && answer != "yes" && answer != "y")
-            {
-                isContinue = false;
-            }
-
+            isContinue = AskToContinue();
             Clear();
         }
 
